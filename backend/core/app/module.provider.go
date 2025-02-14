15a7d25911fe8623051768a72cc69f8438840003
fi44er <fi44er@fi44er.fi44er>
2025-02-14 18:59:06 +0500

    feat: init backend arch

---
+++ backend/core/app/module.provider.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	jwt_module "github.com/root9464/Ton-students/module/jwt"
+)
+
+type moduleProvider struct {
+	// userModule          *user_module.UserModule
+
+	jwtModule *jwt_module.JwtModule
+	app       *App
+}
+
+func NewModuleProvider(app *App) (*moduleProvider, error) {
+	provider := &moduleProvider{
+		app: app,
+	}
+
+	err := provider.initDeps()
+	if err != nil {
+		return nil, err
+	}
+	return provider, nil
+}
+
+func (p *moduleProvider) initDeps() error {
+	inits := []func() error{
+		// p.UserModule,
+	}
+	for _, init := range inits {
+		err := init()
+		if err != nil {
+			p.app.logger.Errorf("%s", "✖ Failed to initialize module: "+err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+// func (p *moduleProvider) UserModule() error {
+// 	p.userModule = user_module.NewUserModule(p.app.logger, p.app.validator, p.app.db, *p.jwtModule, p.app.config.JwtPublicKey)
+// 	return nil
+// }
