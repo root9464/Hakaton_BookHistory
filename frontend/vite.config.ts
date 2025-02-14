96e6ce3294b45bfd09c2b20c7bc224f02ec27120
SooweKun <apextoplega@mail.ru>
2025-02-14 20:19:19 +0500

    upd: alias zustand hero ui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path';
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@components': resolve (__dirname, 'components')
+    },
+  },
 })
