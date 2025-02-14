db1b64d9ff8864c73a820a067734ee93ba7b79fe
fi44er <fi44er@fi44er.fi44er>
2025-02-15 01:48:20 +0500

    upd: fix authorize, add validate auth

--- backend/module/auth/dto/auth.dto.go
+++ backend/module/auth/dto/auth.dto.go
@@ -1,15 +1,15 @@
 package auth_dto
 
 type AutorizeDto struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`    // Обязательное поле, должно быть валидным email
+	Password string `json:"password" validate:"required,min=8"` // Обязательное поле, минимум 8 символов
 }
 
 type RegisterDto struct {
-	Email      string `json:"email"`
-	Password   string `json:"password"`
-	Name       string `json:"name"`
-	Surname    string `json:"surname"`
-	Patronymic string `json:"patronymic"`
-	Phone      string `json:"phone"`
+	Email      string `json:"email" validate:"required,email"`    // Обязательное поле, должно быть валидным email
+	Password   string `json:"password" validate:"required,min=8"` // Обязательное поле, минимум 8 символов
+	Name       string `json:"name" validate:"required"`           // Обязательное поле, только буквы
+	Surname    string `json:"surname" validate:"required"`        // Обязательное поле, только буквы
+	Patronymic string `json:"patronymic" validate:"omitempty"`    // Необязательное поле, только буквы (если указано)
+	Phone      string `json:"phone" validate:"required,e164"`     // Обязательное поле, формат E.164 (например, +79123456789)
 }
