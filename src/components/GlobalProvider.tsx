c71dee9b8db1a815f8f688b58bea2d6fa5752191
iO <senpai9464@yandex.ru>
2025-02-14 20:30:12 +0300

    init 3

--- src/components/GlobalProvider.tsx
+++ src/components/GlobalProvider.tsx
@@ -1,15 +1,16 @@
-import { createRouter, RouterProvider } from '@tanstack/react-router';
-
-// Import the generated route tree
 import { HeroUIProvider } from '@heroui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from '../routeTree.gen';
 
-// Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+  routeTree,
+  context: {
+    userRole: null,
+  },
+});
 const queryClient = new QueryClient();
 
-// Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
     router: typeof router;
@@ -17,6 +18,7 @@ declare module '@tanstack/react-router' {
 }
 
 export const GlobalProvider = () => {
+  // хук для получения значения из store для контекста (лучше использовать store zustand а не чистый функционал tanstack router)
   return (
     <HeroUIProvider>
       <QueryClientProvider client={queryClient}>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
+import svgr from '@svgr/rollup';
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), TanStackRouterVite()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    TanStackRouterVite(),
+    svgr({
+      include: '**/*.svg',
+
+      svgoConfig: {
+        floatPrecision: 2,
+      },
+
+      typescript: true,
+      ref: true,
+      memo: true,
+      svgProps: {
+        ref: 'ref',
+      },
+      prettierConfig: {
+        parser: 'typescript',
+      },
+    }),
+  ],
 });
