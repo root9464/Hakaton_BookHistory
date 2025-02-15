814656e045a311b1c4d46a73fd01f60605f4849d
iO <senpai9464@yandex.ru>
2025-02-15 23:15:24 +0300

    upd: minor

---
+++ frontend/src/pages/AdminPage.tsx
@@ -0,0 +1,10 @@
+export default function AdminPage() {
+  return (
+    <div className='relative flex h-full w-full flex-row items-center justify-between pr-[40px]'>
+      fff
+      <div className='relative flex h-fit w-fit flex-col items-center justify-center'>
+        <p className='absolute bottom-5 text-3xl font-bold text-white'>Цени наших предков !</p>
+      </div>
+    </div>
+  );
+}
