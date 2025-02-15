97c603cf4e1be3b522c98d643dd12da1e8f754ae
ivanbatutin921 <ivanbatutin921@mail.ru>
2025-02-16 04:22:32 +0500

    records

--- backend/module/api/service/api.service.go
+++ backend/module/api/service/api.service.go
@@ -13,7 +13,7 @@ var _ IApiService = (*apiService)(nil)
 
 type IApiService interface {
 	CreateRecord(ctx context.Context, dto *api_dto.CreateFeatureRequest) (*api_dto.CreateFeatureResponse, error)
-	GetRecord(ctx context.Context) ([]api_dto.Feature, error)
+	GetRecord(ctx context.Context) ([]UserResponse, error)
 	GetRecordByID(ctx context.Context, id string) (map[string]interface{}, error)
 	DeleteRecord(ctx context.Context, ids []api_dto.CreateFeatureResponse) error
 	UpdateRecord(ctx context.Context, id string, dto *api_dto.CreateFeatureRequest) error
