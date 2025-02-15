dcda1c437ca0d705b05bd704514fe0105402d887
iO <senpai9464@yandex.ru>
2025-02-15 19:09:09 +0300

    upd: change logic zoom, zoom not dynamic

--- frontend/src/pages/MapPage.tsx
+++ frontend/src/pages/MapPage.tsx
@@ -1,5 +1,5 @@
 import { NavBar } from '@/components/NavBar';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 type Point = { lat: number; lon: number };
 type PixelPosition = { x: number; y: number };
@@ -15,6 +15,7 @@ const project = (lat: number, lon: number, zoom: number) => {
 
 const centerLon = 56.0525;
 const centerLat = 52.3898;
+const zoom = 10;
 
 const points: Point[] = [
   { lat: 52.335844, lon: 56.24909 },
@@ -25,9 +26,8 @@ const points: Point[] = [
 export default function MapPage() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [markers, setMarkers] = useState<PixelPosition[]>([]);
-  const [zoom, setZoom] = useState(10);
 
-  const calculateMarkers = useCallback(() => {
+  useEffect(() => {
     if (!containerRef.current) return;
 
     const centerPixel = project(centerLat, centerLon, zoom);
@@ -48,17 +48,7 @@ export default function MapPage() {
     });
 
     setMarkers(newMarkers);
-  }, [zoom]);
-
-  const handleZoomChange = (newZoom: number) => {
-    setZoom(newZoom);
-  };
-
-  useEffect(() => {
-    calculateMarkers();
-    window.addEventListener('resize', calculateMarkers);
-    return () => window.removeEventListener('resize', calculateMarkers);
-  }, [calculateMarkers, zoom]);
+  }, []);
 
   const handleMarkerClick = (index: number) => {
     console.log(`Клик по маркеру ${index + 1}`, points[index]);
@@ -88,15 +78,6 @@ export default function MapPage() {
           onClick={() => handleMarkerClick(index)}
         />
       ))}
-
-      <div className='absolute bottom-4 right-4 z-20 flex flex-col gap-2'>
-        <button onClick={() => handleZoomChange(zoom + 1)} className='rounded bg-white p-2 shadow-lg'>
-          +
-        </button>
-        <button onClick={() => handleZoomChange(zoom - 1)} className='rounded bg-white p-2 shadow-lg'>
-          -
-        </button>
-      </div>
 
       <div className='absolute top-0 z-[1] h-max w-max bg-transparent'>
         <div className='relative h-full w-full bg-transparent px-2 pt-20 md:px-5'>
