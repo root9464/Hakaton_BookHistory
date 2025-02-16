6693efca3e13091b8ef816b94d1d24c67ab22b07
iO <senpai9464@yandex.ru>
2025-02-16 18:28:40 +0300

    upd: minor

--- frontend/src/modules/auth/components/Header.tsx
+++ frontend/src/modules/auth/components/Header.tsx
@@ -1,6 +1,7 @@
 export const Header = () => (
   <div className='flex h-fit w-fit flex-col items-center justify-center gap-2'>
     <h1 className='text-3xl font-bold'>Регистрация</h1>
+    <p>Книга памяти | Оренбург</p>
     <p className='text-lg font-medium'>– Почти память о своих предках и вспомни историю</p>
   </div>
 );
